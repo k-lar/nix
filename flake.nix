271d465e18a666cb65e075d262c43328b1237a73
K_lar <zan.sprogar@gmail.com>
2026-06-16 01:00:25 +0200

    Added nix-darwin to the config

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 { inputs }:
-let
-  lib = import ./lib { inherit inputs; };
-in
+
 {
   description = "Klar's NixOS flake config";
 
@@ -13,23 +11,40 @@ in
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs:
   let
+    lib = import ./lib { inherit inputs; };
+
     mkSystem = system: modules:
       nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs lib; };
         modules = modules;
       };
+
+    mkDarwin = system: modules:
+      darwin.lib.darwinSystem {
+        inherit system;
+        specialArgs = { inherit inputs lib; };
+        modules = modules;
+      };
   in
   {
-    nixosConfigurations = {
-      klar-nixos = mkSystem "x86_64-linux" [
-        ./hosts/desktop
-        inputs.home-manager.nixosModules.default
-      ];
-    };
+    nixosConfigurations.klar-nixos = mkSystem "x86_64-linux" [
+      ./hosts/desktop
+      inputs.home-manager.nixosModules.default
+    ];
+
+    darwinConfigurations.klar-macbook = mkDarwin "aarch64-darwin" [
+      ./hosts/macbook
+      home-manager.darwinModules.home-manager
+    ];
   };
 }
