ece2500df5d7197d394eb3152b64b19cc68cbece
K_lar <zan.sprogar@gmail.com>
2026-06-22 13:58:31 +0200

    Added shell aliases specific for nix rebuilds and flake updating

--- modules/darwin/shell.nix
+++ modules/darwin/shell.nix
@@ -2,6 +2,14 @@
 
 {
   programs.fish.enable = true;
+  programs.fish = {
+    shellAliases = {
+      nfu = "nix flake update ~/git/nix";
+      nfc = "nix flake check ~/git/nix";
+      nrs = "sudo darwin-rebuild switch --flake ~/git/nix#klar-macbook";
+      nrt = "sudo darwin-rebuild check --flake ~/git/nix#klar-macbook";
+    };
+  };
   users.users.klar.shell = pkgs.fish;
   environment.shells = [ pkgs.fish ];
 }
--- modules/linux/shell.nix
+++ modules/linux/shell.nix
@@ -2,5 +2,14 @@
 
 {
   programs.fish.enable = true;
+  programs.fish = {
+    shellAliases = {
+      nfu = "nix flake update ~/git/nix";
+      nfc = "nix flake check ~/git/nix";
+      nrs = "sudo nixos-rebuild switch --flake ~/git/nix#klar-pc";
+      nrt = "sudo nixos-rebuild test --flake ~/git/nix#klar-pc";
+      nrb = "sudo nixos-rebuild boot --flake ~/git/nix#klar-pc";
+    };
+  };
   users.defaultUserShell = pkgs.fish;
 }
