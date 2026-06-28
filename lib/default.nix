ae78e78b214d1b9a3feeb33c08fa64c0196e4c52
k-lar <sprogarzan@gmail.com>
2026-06-28 10:26:21 +0000

    Removed most of the lib/ stuff since I wasn't using it

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,6 @@
-{ inputs, lib }:
+{ lib, inputs, ... }:
 
 {
-  dotfiles = import ./dotfiles.nix;
-
-  # Detect system type from pkgs if passed, or fall back safely
   isLinux = pkgs: pkgs.stdenv.isLinux;
   isDarwin = pkgs: pkgs.stdenv.isDarwin;
-
-  mkIfLinux = pkgs: condition: value:
-    lib.mkIf (condition && pkgs.stdenv.isLinux) value;
-
-  mkIfDarwin = pkgs: condition: value:
-    lib.mkIf (condition && pkgs.stdenv.isDarwin) value;
 }
