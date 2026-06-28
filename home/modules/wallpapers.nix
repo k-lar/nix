8c72daa4f2ca543bef4f158900b85127df87c5c1
k-lar <sprogarzan@gmail.com>
2026-06-28 10:54:33 +0000

    Download my wallpapers to the correct place on first install / switch

--- home/modules/wallpapers.nix
+++ home/modules/wallpapers.nix
@@ -1,10 +1,15 @@
-{ pkgs }:
+{ config, lib, pkgs, ... }:
 
 {
-  systemd.user.tmpfiles.rules = [
-    "d %h/git 0755 klar users -"
-    "d %h/Pictures/gruvbox_walls 0755 klar users -"
-  ];
+  home.activation.cloneWallpapers =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p "${config.home.homeDirectory}/Pictures"
+
+      if [ ! -d "${config.home.homeDirectory}/Pictures/gruvbox_walls/.git" ]; then
+        ${pkgs.git}/bin/git clone https://gitlab.com/k_lar/gruvbox_walls.git \
+          "${config.home.homeDirectory}/Pictures/gruvbox_walls"
+      fi
+    '';
 
   home.packages = [
     (pkgs.writeShellScriptBin "sync-wallpapers" ''
